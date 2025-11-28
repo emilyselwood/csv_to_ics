3b063580da64d7e2795d7531022c37cc507da43c
Emily S <emilywselwood@gmail.com>
2025-11-28 21:32:10 +0000

    better parameters and column mapping

--- README.md
+++ README.md
@@ -6,3 +6,16 @@ This is designed to be used  with [calendar page](https://github.com/emilyselwoo
 cargo run -- path/to/csv/file.csv path/to/desired/ics/file.ics
 
 ```
+
+Order of the columns shouldn't matter. However it will need timestamps split into date and time columns with the format being yyyy-mm-dd for dates and hh:mm:ss for times.
+
+The following column names will be recognized in the input csv. Any other columns will be ignored.
+
+Column | description | required | format
+-------------------------------
+Name   | Name of the event | True | 
+Start Date | Date that the event starts on | True | yyyy-mm-dd
+Start Time | Time that the event starts | True | hh:mm:ss
+Description | longer description of the event | False |
+End Date | Date that the event ends on | False | yyyy-mm-dd
+End Time | Time that the event ends | False | hh:mm:ss
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,36 @@
-use std::env;
+use std::collections::HashMap;
 
+use clap::{Parser, command};
 use ics::{
     Event, ICalendar,
-    properties::{Description, DtEnd, DtStart, Location, Summary},
+    properties::{Description, DtEnd, DtStart, Summary},
 };
 
+use crate::csv::CSVData;
+
 pub mod csv;
+
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Args {
+    #[arg(index = 1, required = true, help = "Path to input csv file to read")]
+    input_csv: String,
+
+    #[arg(
+        index = 2,
+        required = true,
+        help = "Path to output ics file to write to, will be overwritten by this if it exists."
+    )]
+    output_ics: String,
+
+    #[arg(
+        short,
+        long,
+        help = "Title to use for the calender",
+        default_value = "makers_calendar_uk"
+    )]
+    title: String,
+}
 
 fn date_format(date: &str, time: &str) -> String {
     // 2025-07-19
@@ -34,40 +59,130 @@ fn date_format(date: &str, time: &str) -> String {
 }
 
 fn extract(source: &Vec<char>, start: usize, end: usize) -> String {
+    if source.len() < start || source.len() < end {
+        panic!(
+            "Date format incorrect expected something like '2025-07-19' got {:?}, tried to get characters {} to {} but length is {}",
+            source,
+            start,
+            end,
+            source.len()
+        );
+    }
     return source[start..end].iter().collect::<String>();
 }
 
+fn build_header_map(csv_data: &CSVData) -> HashMap<&str, Option<usize>> {
+    // Name, Start date, Start time, End date, End Time, Description, URL, Latitude, Longitude
+
+    let mut result = HashMap::new();
+    result.insert(
+        "name",
+        csv_data
+            .headers
+            .iter()
+            .position(|h| h.to_lowercase() == "name"),
+    );
+    result.insert(
+        "description",
+        csv_data
+            .headers
+            .iter()
+            .position(|h| h.to_lowercase() == "description"),
+    );
+    result.insert(
+        "start date",
+        csv_data
+            .headers
+            .iter()
+            .position(|h| h.to_lowercase() == "start date"),
+    );
+    result.insert(
+        "end date",
+        csv_data
+            .headers
+            .iter()
+            .position(|h| h.to_lowercase() == "end date"),
+    );
+    result.insert(
+        "start time",
+        csv_data
+            .headers
+            .iter()
+            .position(|h| h.to_lowercase() == "start time"),
+    );
+    result.insert(
+        "end time",
+        csv_data
+            .headers
+            .iter()
+            .position(|h| h.to_lowercase() == "end time"),
+    );
+
+    result
+}
+
+fn get_field(
+    name: &str,
+    row: &Vec<String>,
+    heading_mapping: &HashMap<&str, Option<usize>>,
+) -> Option<String> {
+    heading_mapping
+        .get(name)
+        .and_then(|k| k.map(|i| row[i].clone()))
+}
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args = Args::parse();
 
-    // the first arg is the executable file name.
-    if args.len() != 3 {
-        println!("Requires two positional arguments");
-        println!("csv_to_ics <path/to/csv/file.csv> <path/to/ics/file.ics>");
-        return;
-    }
+    let csv_data = csv::parse_csv_file(&args.input_csv).expect("Could not parse csv file");
 
-    let csv_path = args.get(1).unwrap();
-    let ics_path = args.get(2).unwrap();
+    let heading_mapping = build_header_map(&csv_data);
 
-    let csv_data = csv::parse_csv_file(&csv_path).expect("could not parse csv file");
+    let mut calendar = ICalendar::new("2.0", &args.title);
 
-    let mut calendar = ICalendar::new("2.0", "makers_calendar_uk");
-
-    for row in csv_data.data {
+    for row in &csv_data.data {
         // generate event uid from event name
-        println!("processing {} {} {}", row[0], row[1], row[2]);
+        let name = get_field("name", row, &heading_mapping);
+        let start_date = get_field("start date", row, &heading_mapping);
+        let start_time = get_field("start time", row, &heading_mapping);
 
-        let mut event = Event::new(row[0].clone(), date_format(&row[1], &row[2]));
-        event.push(DtStart::new(date_format(&row[1], &row[2])));
-        event.push(DtEnd::new(date_format(&row[3], &row[4])));
-        event.push(Summary::new(row[0].clone()));
-        event.push(Description::new(row[5].clone()));
+        if name.is_none() || start_date.is_none() || start_time.is_none() {
+            println!(
+                "Row Missing required field Name: '{}' Start_date: '{}' start_time: '{}'",
+                name.or(Some("".to_string())).unwrap(),
+                start_date.or(Some("".to_string())).unwrap(),
+                start_time.or(Some("".to_string())).unwrap()
+            );
+            continue;
+        }
+
+        let name = name.unwrap();
+        let start_date = start_date.unwrap();
+        let start_time = start_time.unwrap();
+
+        println!("processing {} {} {}", &name, &start_date, &start_time);
+
+        let start_date_time = date_format(&start_date, &start_time);
+
+        let mut event = Event::new(name.clone(), start_date_time.clone());
+        event.push(DtStart::new(start_date_time.clone()));
+
+        if let Some(end_date) = get_field("end date", row, &heading_mapping)
+            && let Some(end_time) = get_field("end time", row, &heading_mapping)
+        {
+            let end_date_time = date_format(&end_date, &end_time);
+            event.push(DtEnd::new(end_date_time));
+        }
+        event.push(Summary::new(name.clone()));
+
+        if let Some(description) = get_field("description", row, &heading_mapping) {
+            event.push(Description::new(description.clone()));
+        }
 
         calendar.add_event(event);
     }
 
     calendar
-        .save_file(ics_path)
+        .save_file(&args.output_ics)
         .expect("Could not write ics file");
 }
